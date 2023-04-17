4633a4ff06c366f90cadb1a87071ccd12c6eee6d
AdrijanRadjevic <adrijan.radjevic@gmail.com>
2023-04-18 01:54:39 +0200

    Removed foldername

--- HRplatform/src/main/java/com/adrijan/hrplatform/repo/SkillRepo.java
+++
@@ -1,7 +0,0 @@
-package com.adrijan.hrplatform.repo;
-
-import com.adrijan.hrplatform.models.Skill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SkillRepo extends JpaRepository<Skill, Long> {
-}
